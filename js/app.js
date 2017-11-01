f2d90ba2fb2020504232f8ea241cb0b3567bf9bd
Andrew Hinderaker <andrewhinderaker@gmail.com>
2017-11-01 11:17:55 -0400

    Final Project v 1
    Everything works!

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  $("#menubutton").click(function openMenu() {
+    $("#links").slideToggle();
+  });
+
+  // $("#submit").click(function thankYou() {
+  //   $(".formcontainer").text("Thank you for your message!");
+  // });
+
+});
