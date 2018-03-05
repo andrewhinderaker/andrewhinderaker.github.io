254912dff8ba4e09dbcbcc29c3d598bc17f8b43c
Andrew Hinderaker <andrewhinderaker@gmail.com>
2018-03-04 20:29:30 -0500

    v2
    v2

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
 $(function() {
 
-  $("#menubutton").click(function openMenu() {
-    $("#links").slideToggle();
-  });
-
-  // $("#submit").click(function thankYou() {
-  //   $(".formcontainer").text("Thank you for your message!");
-  // });
+console.log ("js loaded")
 
 });
